fab68650ece366504c61ebc568278a160ba10b75
Alysson Passos <badarass.777@gmail.com>
2023-09-06 20:45:52 -0300

    Criação da Fabrica de Sessão

---
+++ src/test/java/com/pro/alysson/primeiroprojeto/HibernateUtilTest.java
@@ -0,0 +1,13 @@
+package com.pro.alysson.primeiroprojeto;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+public class HibernateUtilTest {
+	@Test
+	public void conectar(){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		sessao.close();
+		HibernateUtil.getFabricaDeSessoes().close();
+	}
+}
